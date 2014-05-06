80b0afd51f2389cc1d913fc30e573b60a2ef5976
jbreeden <jared.breeden@gmail.com>
2014-05-05 22:58:57 -0400

    Update sequel-pad.rb

--- bin/sequel-pad.rb
+++ bin/sequel-pad.rb
@@ -3,13 +3,14 @@ require 'json'
 require 'erb'
 require_relative "../lib/sequel_pad"
 
-# Developer Console
+# Allows hot-loading of files for quick testing & development
 def reload
   Dir.glob("lib/**/*.rb").each { |file|
     load file
   }
 end
 
+# Running pry in the background for exploratory programming
 Thread.new do
   require 'pry'
 end
